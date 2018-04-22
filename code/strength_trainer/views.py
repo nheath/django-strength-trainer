25c9d9d72677ec89fe0d8abc1b0cca5045281e49
jburns24 <jburns24@mail.csuchico.edu>
2018-04-22 13:13:33 -0700

    added check for all workouts finished and redirect to new workout form

--- code/strength_trainer/views.py
+++ code/strength_trainer/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, HttpRequest
 from .forms import registration_form, NewWorkoutForm
 from .models import NewWorkout, User_Profile_Model, WorkoutWeek
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt
 from .utils import create_workout_week
+
 
 def new_workout(request):
     form = NewWorkoutForm()
@@ -32,7 +33,26 @@ def update(request, week, workout):
     if workout == 'deadlift':        
         WorkoutWeek.objects.filter(pk=workout_week.id).update(deadlift_done=True)        
     if workout == 'overhead':        
-        WorkoutWeek.objects.filter(pk=workout_week.id).update(overhead_done=True)        
+        WorkoutWeek.objects.filter(pk=workout_week.id).update(overhead_done=True)     
+    # check if they have any more unfinished workouts
+    weeksdone = 0
+    for single_week in workout_weeks:
+        if single_week.bench_done and single_week.squat_done and single_week.deadlift_done and single_week.overhead_done:
+            weeksdone += 1
+    if weeksdone == 4:
+        # new_max_squat = new_workout.max_squat + 10
+        # NewWorkout.objects.filter(user_id=cur_user.id).update(max_squat=new_max_squat)
+        # new_max_bench = new_workout.max_bench + 5
+        # NewWorkout.objects.filter(user_id=cur_user.id).update(max_bench=new_max_bench)
+        # new_max_deadlift = new_workout.max_deadlift + 10
+        # NewWorkout.objects.filter(user_id=cur_user.id).update(max_deadlift=new_max_deadlift)
+        # new_max_overhead = new_workout.max_overhead + 5
+        # NewWorkout.objects.filter(user_id=cur_user.id).update(max_overhead=new_max_overhead)
+        # new_request = HttpRequest()
+        # new_request.mode = 'POST'
+        # new_request.META = request.META
+        return redirect('/new_workout/')
+        #make a new workout
     return redirect("/home/")
 
 @login_required(login_url="/")
@@ -107,6 +127,29 @@ def home(request):
             workout_multiplier = .4
             create_workout_week(request, workout_multiplier, '_week_4')
             return redirect('/home/')
+        #else:
+            # mod_workout = NewWorkout.objects.filter(user_id=cur_user.id)           
+            # if has_workout > 0:
+            #     new_workout = NewWorkout(
+            #         max_squat = mod_workout.max_squat,
+            #         max_bench = mod_workout.max_bench,
+            #         max_deadlift = mod_workout.max_deadlift,
+            #         max_overhead = mod_workout.max_overhead,
+            #         user_id=cur_user
+            #     )
+            #     NewWorkout.objects.filter(user_id=cur_user.id).delete()
+            #     cur_user_profile.has_workout = 0
+            #     cur_user_profile.save()
+            #     new_workout.save()
+            #     cur_user_profile.has_workout = 1
+            #     cur_user_profile.save()                                
+            #     workout_multiplier = .65
+            #     for i in range(1, 4):
+            #         create_workout_week(request, workout_multiplier, '_week_' + str(i))
+            #         workout_multiplier += .05
+            #     workout_multiplier = .4
+            #     create_workout_week(request, workout_multiplier, '_week_4')
+            #     return redirect('/home/')                
         return render(request, "home.html")
 
 
